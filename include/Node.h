734c43b70c760f4e3efefc03e73935ccf151fdfd
abmkrtchyan <abmkrtchyan@ispras.ru>
2024-02-27 12:34:23 +0400

    add fields color and timestamp, add getters and setters

--- include/Node.h
+++ include/Node.h
@@ -1,12 +1,18 @@
 #ifndef SIMPLE_GRAPH_NODE_H
 #define SIMPLE_GRAPH_NODE_H
 
-#include <functional>
+#include <climits>
+
+enum Color {
+    BLACK, GREY, WHITE
+};
 
 template<class T>
 class Node {
 private:
     T value;
+    Color color = WHITE;
+    std::pair<std::size_t, std::size_t> timestamp{0, INT_MAX};
 
 public:
     Node(const T &val) : value(val) {}
@@ -15,6 +21,30 @@ public:
 
     T getValue() const {
         return value;
+    }
+
+    Color getColor() const {
+        return this->color;
+    }
+
+    void setColor(Color newColor) {
+        this->color = newColor;
+    }
+
+    size_t getStart() const {
+        return this->timestamp.first;
+    }
+
+    size_t getFinish() const {
+        return this->timestamp.second;
+    }
+
+    void setStart(std::size_t start) {
+        this->timestamp.first = start;
+    }
+
+    void setFinish(std::size_t finish) {
+        this->timestamp.second = finish;
     }
 
     bool operator==(const Node &other) const {
