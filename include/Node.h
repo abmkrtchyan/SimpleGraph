8cc44961da663b7bbcd5f016c787edacef06e5b8
abmkrtchyan <abmkrtchyan@ispras.ru>
2024-02-27 12:34:53 +0400

    add print

--- include/Node.h
+++ include/Node.h
@@ -51,6 +51,14 @@ public:
         return value == other.value;
     }
 
+    void print() const {
+        std::cout << "Node: " << getValue()
+                  << ", color: " << (getColor() == BLACK ? "BLACK" : getColor() == GREY ? "GREY" : "WHITE")
+                  << ", start: " << getStart()
+                  << ", finish: " << getFinish()
+                  << std::endl;
+    }
+
     struct HashFunction {
         size_t operator()(const Node<T> &node) const {
             return std::hash<T>()(node.getValue());
